0ec055cdbfc98e613503e75c22e2f78fa8603787
rudolf1 <rudolfss13@gmail.com>
2025-08-12 20:23:21 +0200

    port change

--- backend/app.py
+++ backend/app.py
@@ -153,4 +153,4 @@ if __name__ == '__main__':
     processing.start()
 
     # Run the server
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=5001, debug=False, allow_unsafe_werkzeug=True)
