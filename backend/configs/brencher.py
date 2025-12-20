4d7a61109ff4fcf364b8636f096ca3f5b7c83ebe
rudolf <rudolfss13@gmail.com>
2025-12-20 20:45:51 +0100

    Import checkPingF function from brencher2 to enhance pipeline functionality

--- backend/configs/brencher.py
+++ backend/configs/brencher.py
@@ -4,7 +4,7 @@ from steps.docker import DockerComposeBuild, DockerSwarmCheck, DockerSwarmDeploy
 from enironment import Environment
 from typing import List, Dict, Any, Optional, Tuple
 from steps.step import AbstractStep
-from brencher2 import checkPingF
+import checkPingF
 
 env = Environment(
     id="brencher",
